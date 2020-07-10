09a14ec68ca21886b8ea5ab52f28695f8d2ce4cc
TamtamHero <10632523+TamtamHero@users.noreply.github.com>
2020-07-10 14:46:56 +0200

    Harmonize base menu UI with other Ledger apps

--- src/ui_idle.c
+++ src/ui_idle.c
@@ -2,16 +2,35 @@
 #include "os_io_seproxyhal.h"
 #include "algo_ui.h"
 
-UX_STEP_NOCB(ux_idle_flow_1_step, bn, {"Version", APPVERSION});
-UX_STEP_NOCB_INIT(ux_idle_flow_2_step, bnnn_paging, step_address(), {"Address", text});
-UX_STEP_VALID(ux_idle_flow_3_step, pb, os_sched_exit(-1), {&C_icon_dashboard, "Quit"});
-
-const ux_flow_step_t * const ux_idle_flow [] = {
-  &ux_idle_flow_1_step,
-  &ux_idle_flow_2_step,
-  &ux_idle_flow_3_step,
-  FLOW_END_STEP,
-};
+UX_FLOW_DEF_NOCB(
+    ux_idle_flow_welcome_step,
+    nn, //pnn,
+    {
+      //"", //&C_icon_dashboard,
+      "Application",
+      "is ready",
+    });
+UX_FLOW_DEF_NOCB(
+    ux_idle_flow_version_step,
+    bn,
+    {
+      "Version",
+      APPVERSION,
+    });
+UX_FLOW_DEF_VALID(
+    ux_idle_flow_exit_step,
+    pb,
+    os_sched_exit(-1),
+    {
+      &C_icon_dashboard_x,
+      "Quit",
+    });
+UX_FLOW(ux_idle_flow,
+  &ux_idle_flow_welcome_step,
+  &ux_idle_flow_version_step,
+  &ux_idle_flow_exit_step,
+  FLOW_LOOP
+);
 
 UX_STEP_NOCB(ux_loading_1_step, bn, {"Loading...", "Please wait"});
 
